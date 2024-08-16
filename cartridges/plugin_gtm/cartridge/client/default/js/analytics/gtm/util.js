f0cbfd768f3954a41c996254961cc13ffd61f014
Oleksandr Olimbiovskyi <o.olimbiovskyi@gmail.com>
2024-08-16 13:44:53 +0300

    ADD: Client-side optimization

--- cartridges/plugin_gtm/cartridge/client/default/js/analytics/gtm/util.js
+++ cartridges/plugin_gtm/cartridge/client/default/js/analytics/gtm/util.js
@@ -16,6 +16,35 @@ module.exports = {
     },
 
     /**
+     * Recursively merges properties of source objects into the target object.
+     * If a property is an array or an object, the merge is done deeply.
+     * Arrays are concatenated, and objects are merged recursively.
+     * @param {Object} target - The target object that will receive the properties.
+     * @param {...Object} sources - One or more source objects whose properties will be merged into the target.
+     * @returns {Object} The target object after merging.
+     */
+    deepAssign(target, ...sources) {
+        return sources
+            .filter((obj) => obj !== null && obj !== undefined)
+            .reduce((prev, obj) => {
+                Object.keys(obj).forEach((key) => {
+                    const pVal = prev[key];
+                    const oVal = obj[key];
+
+                    if (Array.isArray(pVal) && Array.isArray(oVal)) {
+                        prev[key] = [...pVal, ...oVal];
+                    } else if (typeof pVal === 'object' && typeof oVal === 'object') {
+                        prev[key] = deepAssign({ ...pVal }, oVal);
+                    } else {
+                        prev[key] = oVal;
+                    }
+                });
+
+                return prev;
+            }, target);
+    },
+
+    /**
      * Retrieves data from a script tag with a specific type.
      * @param {string} type - The type of the data to retrieve.
      * @returns {Object|null} The parsed data object or null if parsing fails.
